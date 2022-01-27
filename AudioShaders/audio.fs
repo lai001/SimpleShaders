139b0a4e4644e9bcb3d879846ed6cf415cbea7b1
lai001 <1104698042@qq.com>
2022-01-27 23:25:43 +0800

    Create audio.fs

---
+++ AudioShaders/audio.fs
@@ -0,0 +1,17 @@
+void mainImage(out vec4 fragColor, in vec2 fragCoord) 
+{
+    float bans = 40.0;
+    vec2 uv = fragCoord / iResolution.xy;
+    vec3 color = mix(vec3(1.0, 0.0, 0.0), vec3(1.0, 165.0 / 255.0, 0.0), uv.y);
+    float frequency = texture(iChannel0, vec2(floor(uv.x * bans) / bans, 0.0)).x;
+    if(uv.y > frequency) 
+    {
+        fragColor = vec4(vec3(0.0), 1.0);
+    } 
+    else 
+    {
+        float mask = step(0.2, fract(uv.x * bans));
+        float r = 1.0 + smoothstep(0.0, frequency, uv.y);
+        fragColor = vec4(r * color * mask, 1.0);
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 环境:   
 Visual Studio Code  
 Shader Toy 扩展
+
+Shader Toy 扩展不支持音频加载，AudioShaders目录下的仅用作副本保存
